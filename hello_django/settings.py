7f8948833b138a782aca91156e9a530ea2e64528
ADMIN <pt27122000@gmail.com>
2021-10-26 00:35:41 +0700

    a

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q70e^jr@yo&e$w2ap&*aevwzrwu!9f=v&om98!5w5qm@t8(b+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
